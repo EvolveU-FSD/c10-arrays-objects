d3aa30a0e475a10ab2ab834ae315f679a71bdd53
Chris Desmarais <desmarais_chris@hotmail.com>
2023-03-04 15:22:33 -0700

    My copy

--- wakeUp.js
+++
@@ -1,8 +0,0 @@
-const wakeUp = (extraTime) => {
-    console.log("wake up");
-    console.log("do I have extra time?");
-    if (extraTime) {
-      console.log("Watch some YouTube");
-    }
-  };
-  exports.wakeUp = wakeUp;
